f5bc4b38525422a79a5c0fdc8a3e4113e5822671
andiwillfly <andiwillfly@gmail.com>
2016-03-31 20:01:52 +0300

    Fix aside scss styles and html structure

--- pages/Aside.js
+++ pages/Aside.js
@@ -6,56 +6,57 @@ class Aside extends React.Component {
 	render() {
 
 		return (
-			<div className="sidebar header">
-				<h1 className="sidebar-aside-logo">
-					<a className="sidebar-aside-logo-link">DOBROE IT</a>
+			<div className="sidebar">
+
+				<h1 className="site-logo">
+					<a className="site-logo__link">DOBROE IT</a>
 				</h1>
 
-				<div className="box sidebar-aside-nav">
-					<ul>
-						<li className="sidebar-aside-nav-first-aside-link">
-							<Link to={ `/${this.props.params.lang}` }>{ i18n.t('Main') }</Link>
-						</li>
-						<li>
-							<Link to={ '/' + this.props.params.lang + '/resume' }>Resume</Link>
-						</li>
-						<li>
-							<Link to={ `/${this.props.params.lang}/portfolio` }>Portfolio</Link>
-						</li>
-						<li>
-							<Link to={ `/${this.props.params.lang}/skills` }>Skills</Link>
-						</li>
-						<li>
-							<a href="#content-news-services">Services</a>
-						</li>
-						<li>
-							<a href="#content-news-way">My way</a>
-						</li>
-						<li>
-							<Link to={ `/${this.props.params.lang}/feedback` }>To order a site</Link>
-						</li>
-						<li>
-							<Link to={ `/${this.props.params.lang}/contacts` }>Contacts</Link>
-						</li>
-					</ul>
+				<ul className="box navigation">
+					<li className="navigation__item">
+						<Link to={ `/${this.props.params.lang}` }>{ i18n.t('Main') }</Link>
+					</li>
+					<li className="navigation__item">
+						<Link to={ `/${this.props.params.lang}/resume` }>{ i18n.t('Resume') }</Link>
+					</li>
+					<li className="navigation__item">
+						<Link to={ `/${this.props.params.lang}/portfolio` }>{ i18n.t('Portfolio') }</Link>
+					</li>
+					<li className="navigation__item">
+						<Link to={ `/${this.props.params.lang}` }>{ i18n.t('Skills') }</Link>
+					</li>
+					<li className="navigation__item">
+						<Link to={ `/${this.props.params.lang}` }>{ i18n.t('Services') }</Link>
+					</li>
+					<li className="navigation__item">
+						<Link to={ `/${this.props.params.lang}` }>{ i18n.t('My way') }</Link>
+					</li>
+					<li className="navigation__item">
+						<Link to={ `/${this.props.params.lang}` }>{ i18n.t('To order a site') }</Link>
+					</li>
+					<li className="navigation__item">
+						<Link to={ `/${this.props.params.lang}` }>{ i18n.t('Contacts') }</Link>
+					</li>
+				</ul>
 
-				</div>
-
-				<div className="box sidebar-aside-search">
-					<form method="post" action="#">
-						<input type="text" className="sidebar-aside-search-text-input" name="search" placeholder="Search" />
+				<div className="box search">
+					<form>
+						<input type="text" className="search__input" name="search" placeholder="Search"/>
 					</form>
 				</div>
-				<div className="box sidebar-aside-tags">
-					<div className="sidebar-aside-tags-inside">
-						<p><strong>Striped:</strong>
+
+				<div className="box tags">
+					<div className="tags__inner">
+						<p>
+							<strong>Striped:</strong>
 							A free and fully responsive HTML5 site template designed by
 							<a>AJ</a> for
 							<a>HTML5 UP</a>
 						</p>
 					</div>
 				</div>
-				<div className="box sidebar-aside-recent-posts">
+
+				<div className="box recent-posts">
 					<h3>Recent Posts</h3>
 					<ul>
 						<li>
@@ -74,9 +75,9 @@ class Aside extends React.Component {
 							<a>Malesuada commodo</a>
 						</li>
 					</ul>
-
 				</div>
-				<div className="box sidebar-sidebar-recent-comments">
+
+				<div className="box recent-comments">
 					<h3>Recent Comments</h3>
 					<ul>
 						<li>case on <a>Lorem ipsum dolor</a></li>
@@ -84,7 +85,8 @@ class Aside extends React.Component {
 						<li>case on <a>Sed dolore magna</a></li>
 					</ul>
 				</div>
-				<div className="box calendar-aside"></div>
+
+				<div className="box calendar"></div>
 				<ul className="copyright"></ul>
 			</div>
 		);
